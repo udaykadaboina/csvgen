c7c33c5b7527ada4b17df16cb8decf26bff7455b
Uday Kadaboina <uday88k@gmail.com>
2012-10-07 18:59:18 -0600

    also calculates the time

---
+++ csv.rb
@@ -0,0 +1,12 @@
+
+	
+	
+		CSV.open("C:\\Users\\Uday\\thesis_code\\csvfile.csv", "wb") do |csv|
+  		csv << ["row", "of", "CSV", "data"]
+  		csv << ["another", "row"]
+  		# ...
+		end	
+	
+
+
+
--- csvgen.rb
+++ csvgen.rb
@@ -3,16 +3,20 @@ class Csvgen
         @my_interval
         @jersey_rand = [ 3, 5 , 11, 13, 22, 23, 99, 10, 12, 8 ]
         @win_lose
+        @tcurrent
+    
 
     end
 #include Threads
     def start
+        @tcurrent = 0
         t = Thread.new do
             while true do
                 @my_interval = rand(9)
             #    puts "random num: is #{@my_interval}"
-            #    puts "next num will be generated in #{@my_interval} seconds"                
-                puts "#{[ 3, 5 , 11, 13, 22, 23, 99, 10, 12, 8 ].sample} has #{[:won, :lost].sample} 50-50"
+            #    puts "next num will be generated in #{@my_interval} seconds" 
+                @tcurrent += @my_interval               
+                puts "#{[ 3, 5 , 11, 13, 22, 23, 99, 10, 12, 8 ].sample} has #{[:won, :lost].sample} 50-50 at #{@tcurrent}"
                 sleep @my_interval
             end
         end
@@ -24,3 +28,5 @@ end
 gen = Csvgen.new
 
 gen.start
+
+
