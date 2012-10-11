a9c8d98a131f92c65d94fedb36673b50d687178d
Uday Kadaboina <uday88k@gmail.com>
2012-10-11 00:41:44 -0600

    stripped the Excel intelligence, showing till 70 mins

--- possession.rb
+++ possession.rb
@@ -8,14 +8,20 @@ class Csvposgen
     def start
         @tcurrent = 0
         t = Thread.new do
-            CSV.open("p_list.csv", "wb") do |csv|
+            CSV.open("p_list2.csv", "wb") do |csv|
                 mm = 0
-                until mm == 60 do 
+                until mm == 70 do 
                 interval = 30
                 @tcurrent += interval            
                 mm,ss = @tcurrent.divmod(60)
                 pos = rand(30) + 40
-                a = ["00:#{mm}:#{ss}", pos]
+                if ss == 0 then
+                    ss = "00"
+                
+                elsif
+                   ss = ss
+                end             
+                a = [" #{mm}:#{ss}", pos]
                 puts a
                 csv << a
                 end
