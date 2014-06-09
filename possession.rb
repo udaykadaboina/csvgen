f4b11a80a343797dd13caf45751ae3aacf82a755
Uday <uday88k@gmail.com>
2014-06-09 17:17:09 -0400

    cleaned the code

--- possession.rb
+++ possession.rb
@@ -1,37 +1,31 @@
 require 'CSV'
-class Csvposgen
-    def initialize
-        @tcurrent
-    end
-
-# include Threads
-    def start
-        @tcurrent = 0
-        t = Thread.new do
-            CSV.open("p_list2.csv", "wb") do |csv|
-                mm = 0
-                until mm == 70 do 
-                interval = 30
-                @tcurrent += interval            
-                mm,ss = @tcurrent.divmod(60)
-                pos = rand(30) + 40
-                if ss == 0 then
-                    ss = "00"
-                
-                elsif
-                   ss = ss
-                end             
-                a = [" #{mm}:#{ss}", pos]
-                puts a
-                csv << a
-                end
-            end
-        end #thread
-
-
-    end #start
+class Possession
+  # Every 30 secs a random posession # will be created which is used to create a csv file & create a graph using.
+  # use of Threads
+  def start
+    tcurrent = 0
+    t = Thread.new do
+      CSV.open("p_list.csv", "wb") do |csv|
+        mm = 0
+        until mm == 70 do 
+        interval = 30
+        tcurrent += interval            
+        mm,ss = tcurrent.divmod(60)
+        pos = rand(30) + 40 #creates random posession #.
+        if ss == 0 then
+          ss = "00"
+        elsif
+          ss = ss
+        end             
+        a = [" #{mm}:#{ss}", pos]
+        puts a
+        csv << a
+        end
+      end
+      end #thread
+  end #start
  
 end #class
 
-gen = Csvposgen.new
+gen = Possession.new
 gen.start
